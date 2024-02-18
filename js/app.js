460f91d14ac0eca137bd57c68d6bedd1b24965cf
NABILA CHOWDHURY NITU <155249879+chowdhurynabila@users.noreply.github.com>
2024-02-18 20:01:00 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const kbd = document.getElementsByClassName("kbd");  
+const kbd = document.getElementsByClassName("kbd");
 
 let totalOrder = 0;
 
@@ -6,11 +6,16 @@ for (const el of kbd) {
   el.addEventListener("click", (el) => {
     totalOrder += 1;
     setInnerText("totalSeat", totalOrder);
+
+    const color = el.target;
+    color.style.backgroundColor = "green";
+    color.style.color = "white";
     const div = document.createElement("div");
     const h3 = document.createElement("h3");
     h3.innerText = el.target.innerText;
     const p = document.createElement("p");
     p.innerText = "Economoy";
+
     const span = document.createElement("span");
     const price = (span.innerText = 550);
 
@@ -19,10 +24,15 @@ for (const el of kbd) {
     div.appendChild(span);
     const orderdiv = document.getElementById("order");
     orderdiv.appendChild(div);
+
     const seatLeft = document.getElementById("seatLeft");
     const seatLeftValue = parseInt(seatLeft.innerText);
     const totalSeatLeft = seatLeftValue - 1;
     seatLeft.innerText = totalSeatLeft;
+
+    const totalSeat = document.getElementById("totalSeat");
+
+    const totalSeatConverted = parseInt(totalSeat.innerText);
 
     const totalPrice = document.getElementById("totalPrice");
     const totalPriceValue = parseInt(totalPrice.innerText);
@@ -37,15 +47,21 @@ for (const el of kbd) {
         const grandTotal = document.getElementById("grandTotal");
         const grandTotalValueConverted = parseInt(grandTotal);
         const totalGrand = (totalePriceTotal * 15) / 100;
+        const discountTotal = document.getElementById("discountPrice");
+        discountTotal.innerText = totalGrand;
         const total = totalePriceTotal - totalGrand;
         grandTotal.innerText = total;
+        disableBtn("inputField");
       }
       if (searchValue === "Couple20") {
         const grandTotal = document.getElementById("grandTotal");
         const grandTotalValueConverted = parseInt(grandTotal);
         const totalGrand = (totalePriceTotal * 20) / 100;
+        const discountTotal = document.getElementById("discountPrice");
+        discountTotal.innerText = totalGrand;
         const total = totalePriceTotal - totalGrand;
         grandTotal.innerText = total;
+        disableBtn("inputField");
       } else {
         alert("please enter correct code");
         return;
@@ -57,11 +73,6 @@ for (const el of kbd) {
 function setInnerText(id, value) {
   document.getElementById(id).innerText = value;
 }
-// function setbgColor(id) {
-//   const bgColor = document.getElementById(id);
-//   bgColor.classList.add("bg-green-500");
-//   bgColor.classList.add("text-white");
-// }
 
 function next() {
   const mainbody = document.getElementById("mainbody");
@@ -71,4 +82,8 @@ function next() {
 }
 function continuebtn() {
   location.reload();
+}
+
+function disableBtn(id) {
+  document.getElementById(id).disabled = true;
 }
